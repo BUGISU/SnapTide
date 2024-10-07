9ab015bfc853e6a241db78f3eb354e320a444e76
별찌루 <38304918+JISUSAMA@users.noreply.github.com>
2024-10-07 14:03:07 +0900

    20241007

--- insta_front/src/pages/members/Login.tsx
+++ insta_front/src/pages/members/Login.tsx
@@ -1,5 +1,5 @@
-import type {ChangeEvent} from 'react'
-import {useState, useCallback, useEffect} from 'react'
+import type {ChangeEvent, FormEvent} from 'react'
+import {useState, useCallback, useEffect, useRef} from 'react'
 import {Link, useNavigate} from 'react-router-dom'
 import {useAuth} from '../../contexts'
 import * as U from '../../utils'
@@ -25,42 +25,86 @@ export default function Login() {
     })
   }, [email, pw, navigate, login])
 
+  const emailRef = useRef<HTMLInputElement>(null)
+  const pwRef = useRef<HTMLInputElement>(null)
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    if (emailRef.current?.value === '') {
+      alert('Please Check Email')
+      if (emailRef.current !== null) emailRef.current.focus()
+      return
+    }
+    if (pwRef.current?.value === '') {
+      alert('Please Check Password')
+      if (pwRef.current !== null) {
+        pwRef.current.focus()
+      }
+      return
+    }
+    getSession(email, pw)
+  }
+  const getSession = async (email: string, pw: string) => {
+    try {
+      new Promise((resolve, reject) => {
+        // prettier ignore
+        fetch('http://localhost:8080/api/auth/login?email=' + email + '&pw=' + pw, {
+          method: 'POST'
+        })
+          .then(res => res.text())
+          .then(token => {
+            console.log(token)
+          })
+          .catch(err => console.log('Error:', err))
+      })
+    } catch (error) {
+    } finally {
+    }
+  }
   useEffect(() => {
     U.readObjectP<LoginFormType>('user')
       .then(user => {
         if (user) setForm(user)
       })
       .catch(e => {})
+    emailRef.current?.focus()
   }, [])
   return (
-    <div className="flex flex-col min-h-screen bg-gray-100 border border-gray-300 shadow-xl rounded-xl">
+    <div className="flex flex-col pt-2 bg-gray-100 border border-gray-300 shadow-xl rounded-xl">
       <div className="flex flex-col items-center justify-center flex-1 max-w-sm px-2 mx-auto">
         <div className="w-full px-6 py-8 text-black bg-white rounded shadow-md">
-          <h1 className="mb-8 text-2xl text-center text-primary">Login</h1>
-          <input
-            type="text"
-            name="email"
-            className="w-full p-3 mb-4 input input-primary"
-            placeholder="Email"
-            value={email}
-            onChange={changed('email')}
-          />
-          <input
-            type="password"
-            name="pw"
-            className="w-full p-3 mb-4 input input-primary"
-            placeholder="Password"
-            value={pw}
-            onChange={changed('pw')}
-          />
-          <button type="submit" className="w-full btn btn-primary" onClick={loginAccount}>
-            Login
-          </button>
+          <form method="post" onSubmit={onSubmit}>
+            <h1 className="mb-8 text-2xl text-center text-primary">Login</h1>
+            <input
+              type="text"
+              name="email"
+              ref={emailRef}
+              className="w-full p-3 mb-4 input input-primary"
+              placeholder="Email"
+              value={email}
+              onChange={changed('email')}
+            />
+            <input
+              type="password"
+              name="pw"
+              ref={pwRef}
+              className="w-full p-3 mb-4 input input-primary"
+              placeholder="Password"
+              value={pw}
+              onChange={changed('pw')}
+            />
+            <button
+              type="submit"
+              className="w-full btn btn-primary"
+              onClick={loginAccount}>
+              Login
+            </button>
+          </form>
         </div>
         <div className="mt-6 text-grey-dark">
           Create account?
-          <Link className="btn btn-primary btn-link" to="/signup">
-            Login
+          <Link className="btn btn-primary btn-link" to="/join">
+            Join
           </Link>
         </div>
       </div>
